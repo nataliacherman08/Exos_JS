52c305022cd4b368d472b7c54ca7cb46b9aa0253
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-02 16:09:33 +0100

    Update

--- 01.Season-1/04-dates/04-locale-date/script.js
+++ 01.Season-1/04-dates/04-locale-date/script.js
@@ -6,6 +6,34 @@
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+    let date = new Date();
+
+    //MONTH-----------------------------------------------------------
+    const months = ['January','February','March','April',
+                    'May','June','July','August',
+                    'September','October','November','December'];
+    const monthsIndex = date.getMonth();
+    const month = months[monthsIndex];
+
+    //the day of the week----------------------------------------------
+    const days = ['Sunday','Monday','Tuesday','Wednesday',
+                'Thursday','Friday','Saterday'];
+    const day = days[date.getDay()];
+
+    //YEAR--------------------------------------------------------------
+    let year = date.getFullYear();
+
+    //the day of the month----------------------------------------------
+    let n = date.getDate();
+                
+    //HOUR--------------------------------------------------------------
+    let hour = date.getHours();
+    //Minutes-----------------------------------------------------------
+    let minute = date.getMinutes();
+
+    //Complete textual date
+    const dateOfToday = day + ' ' + n + ' ' + month + ' ' + year + ', ' + hour + 'h' + minute;
+
+    document.getElementById("target").innerHTML = dateOfToday;
 
 })();
