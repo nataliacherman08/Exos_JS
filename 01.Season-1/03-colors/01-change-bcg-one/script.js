7d374e8e476dac97e57bcd63cd7002c59dd76fb0
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-02 14:04:18 +0100

    Update color

--- 01.Season-1/03-colors/01-change-bcg-one/script.js
+++ 01.Season-1/03-colors/01-change-bcg-one/script.js
@@ -4,6 +4,32 @@
 
 (() => {
 
-    // your code here
+    // background color red
+    document.getElementById('red').onclick = bgRed;
+
+    function bgRed() {
+        document.body.style.backgroundColor = "#bb010b";
+    }
+
+    // background color green
+    document.getElementById('green').onclick = bgGreen;
+
+    function bgGreen() {
+        document.body.style.backgroundColor = "#02a570";
+    }
+
+    // background color yellow
+    document.getElementById('yellow').onclick = bgYellow;
+
+    function bgYellow() {
+        document.body.style.backgroundColor = "#ffdb58";
+    }
+
+    // background color blue
+    document.getElementById('blue').onclick = bgBlue;
+
+    function bgBlue() {
+        document.body.style.backgroundColor = "#005C96";
+    }
 
 })();
