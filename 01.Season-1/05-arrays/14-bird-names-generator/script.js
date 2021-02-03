499ad7d77a21d9526f76977beb49c09c156c8fad
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-03 15:08:41 +0100

    Update

--- 01.Season-1/05-arrays/14-bird-names-generator/script.js
+++ 01.Season-1/05-arrays/14-bird-names-generator/script.js
@@ -3,18 +3,18 @@
 
 (() => {
     const birds = [
-        {name: "mouette", fem: true},
-        {name: "corbeau"},
-        {name: "mésange", fem: true},
-        {name: "hibou"},
-        {name: "buse", fem: true},
-        {name: "pigeon"},
-        {name: "pie", fem: true},
-        {name: "vautour"},
-        {name: "faucon"},
-        {name: "rouge-gorge"},
-        {name: "tourterelle", fem: true},
-        {name: "corneille", fem: true},
+        { name: "mouette", fem: true },
+        { name: "corbeau" },
+        { name: "mésange", fem: true },
+        { name: "hibou" },
+        { name: "buse", fem: true },
+        { name: "pigeon" },
+        { name: "pie", fem: true },
+        { name: "vautour" },
+        { name: "faucon" },
+        { name: "rouge-gorge" },
+        { name: "tourterelle", fem: true },
+        { name: "corneille", fem: true },
     ];
     const adjectives = new Set([
         "cendré",
@@ -30,5 +30,20 @@
         "arboré",
     ]);
 
-    // your code here
+    document.getElementById('run').addEventListener('click', () => {
+        let randomBird = birds[Math.floor(Math.random() * birds.length)];
+        console.log(randomBird);
+
+        //It doesn't work with a Set so I create a new array and take the elements from the Set 'adjectives'
+        let adjectivesArray = Array.from(adjectives);
+        let randomAdjective = adjectivesArray[Math.floor(Math.random() * adjectivesArray.length)];
+        console.log(randomAdjective);
+
+        if (randomBird.fem === true) {
+            document.getElementById('target').innerHTML = (randomBird.name + " " + randomAdjective + "e");
+        } else {
+            document.getElementById('target').innerHTML = (randomBird.name + " " + randomAdjective);
+        }
+    });
+
 })();
