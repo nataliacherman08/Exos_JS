0e22785743ec544fd8cdade7bfdf99d27dbc6dba
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-04 19:43:59 +0100

    Update

--- 01.Season-1/08-dom/11-change-event-input-one/script.js
+++ 01.Season-1/08-dom/11-change-event-input-one/script.js
@@ -3,6 +3,18 @@
 
 (() => {
 
-    // your code here
+    let password = document.getElementById('pass-one');
+    let counterSpan = document.getElementById('counter');
+
+    password.addEventListener('input', () => {
+
+        (password.value.length > 10) &&
+            (password.value = password.value.substring(0, password.value.length - 1))
+
+        //If the user adds an element to the password, the counter span add 1
+        counterSpan.innerHTML = password.value.length + '/10';
+
+
+    });
 
 })();
