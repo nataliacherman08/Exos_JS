3cf3a5337ccf220d81df4f6f4a710261a5e6b3d0
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-04 12:37:34 +0100

    Update

--- 01.Season-1/08-dom/04-attr-create/script.js
+++ 01.Season-1/08-dom/04-attr-create/script.js
@@ -3,6 +3,15 @@
 
 (() => {
 
-    // your code here
+    //Get the data-image by Source Id
+    let sourceId = document.getElementById('source');
+    let data = sourceId.getAttribute('data-image');
+
+    //Go to the Target Id and replace the src by the data variable
+    let target = document.getElementById('target');
+    target.innerHTML = `<img src=${data} />`;
+
+    //Remove the original element : parent.removeChild(child); (Not sure about this part.)
+    material.removeChild(target);
 
 })();
