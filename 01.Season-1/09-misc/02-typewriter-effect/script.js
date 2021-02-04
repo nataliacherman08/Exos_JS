670cfdb88fecd0c65e868baaf28bcc2f71fd85f6
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-04 21:18:13 +0100

    Update

--- 01.Season-1/09-misc/02-typewriter-effect/script.js
+++ 01.Season-1/09-misc/02-typewriter-effect/script.js
@@ -3,6 +3,25 @@
 
 (() => {
 
-    // your code here
+    //Speed
+    let speed = 150;
+    let i = 0;
+
+    //Retrieve and hide the text from HTML
+    let text = document.getElementById('target').textContent;
+    let target = document.getElementById('target');
+    target.innerHTML = '';
+
+    //Type Writer Effect
+    function typeWriter() {
+        if (i < text.length) {
+            target.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        };
+    };
+
+    //Activate the function
+    typeWriter();
 
 })();
