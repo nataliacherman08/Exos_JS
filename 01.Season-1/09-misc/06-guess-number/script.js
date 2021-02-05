e14e3a48d1b98fc4c95566c248ee072925617c5b
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-05 09:26:30 +0100

    Update

--- 01.Season-1/09-misc/06-guess-number/script.js
+++ 01.Season-1/09-misc/06-guess-number/script.js
@@ -5,21 +5,25 @@
     //Generate a random number
     let x = [Math.floor(Math.random() * 100) + 1];
     //User's input number
-    let userNumber = '';
+    let number = '';
     //User's guesses
     let y = 0;
+    y += 1;
 
 
     //Create a loop as long as the user's input number is false
-    while (userNumber !== x) {
-        userNumber = parseInt(prompt('Can you guess a number between 1 and 100 ?'));
+    while (number !== x) {
+        number = parseInt(prompt('Can you guess a number between 1 and 100 ?'));
+        //Counter of user's guesses
         y += 1;
 
-        if (userNumber > x) {
-            alert("You guessed to high !");
-        } else if (userNumber < x) {
-            alert("You guessed too low !");
+        //Conditions to indicate the user if he is under or above 
+        if (number > x) {
+            alert('Lower');
+        } else if (number < x) {
+            alert('Higher');
         }
+
         //If the user finds the random number
         else {
             alert("That's it! As well as you needed " + y + " guesses");
