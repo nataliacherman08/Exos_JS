4743eb78a6064a443717ccafb404e16d97e0f248
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-05 09:43:44 +0100

    Update script.js

--- 01.Season-1/09-misc/07-storage-clicker/script.js
+++ 01.Season-1/09-misc/07-storage-clicker/script.js
@@ -3,6 +3,21 @@
 
 (() => {
 
-    // your code here
+    function counter() {
+        if (typeof (Storage) !== 'undefined') {
+            if (localStorage.clickcount) {
+                localStorage.clickcount = Number(localStorage.clickcount) + 1;
+            } else {
+                localStorage.clickcount = 1;
+            }
+            document.querySelector('.counter').innerText = 'You have clicked on the button ' + localStorage.clickcount + ' time(s)';
+        } else {
+            document.querySelector('.counter').innerText = 'Sorry, something went wrong.';
+        }
+    }
+
+    document.getElementById('increment').addEventListener('click', () => {
+        counter()
+    });
 
 })();
