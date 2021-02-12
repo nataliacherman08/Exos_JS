409f7d1a0289a2653325bb51ccc786f17fe1e5ab
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 12:39:33 +0100

    Update 1.1

--- 01.Season-1/01-base/01-var-alert/script.js
+++ 01.Season-1/01-base/01-var-alert/script.js
@@ -5,7 +5,8 @@
     console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
     console.log('Hello Woods programmer');
 
-    // YOUR CODE HERE
-    let hello = 'Hello, World !';
-    alert(hello);
+    // 1) Create a variable + set value to be a string
+    let greet = "Hello, World!";
+    //2) Display this variable in a dialog box (alert box)
+    alert(greet);
 })();
