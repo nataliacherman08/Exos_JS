469157e24ac5e3cf00c7bfd4642285dc3a55c12d
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 12:53:17 +0100

    Update 1.3

--- 01.Season-1/01-base/03-prompt-if/script.js
+++ 01.Season-1/01-base/03-prompt-if/script.js
@@ -6,12 +6,12 @@
   console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
   console.log('Hello Woods programmer');
 
-  // YOUR CODE HERE
-  let cake = prompt('Do want some cake ?');
-  if (cake === 'yes' | cake === 'yes, please' | cake === 'Yes' |
-  cake === 'Yes, please' | cake === 'Of course') {
-    alert('Congratulations !');
-  }    else {
-    alert(' More cake for me then :p !');
+  //1) Ask the user if he want some cake
+  let proposal = prompt("Do you want some cake?");
+  //2) Setting up the conditions
+  if ((proposal == "yes") || (proposal == "yes, of course")) {
+    alert("Congratulations!");
+  } else {
+    alert("More cake for me then :p !");
   }
 })();
