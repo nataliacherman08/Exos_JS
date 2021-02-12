abbce73732e6c87b58e6c43271ef26f23c7a9c2a
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 14:50:38 +0100

    Update 2.2

--- 01.Season-1/02-maths/02-calculator-two/script.js
+++ 01.Season-1/02-maths/02-calculator-two/script.js
@@ -4,33 +4,46 @@
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
 
+
+    // Creation of a function that will perform the operations
     const performOperation = operation => {
+
+        //Define variables as numbers and values (from inputs)
         let a = parseInt(document.getElementById("op-one").value);
         let b = parseInt(document.getElementById("op-two").value);
+
+        //Switch (check the value of a variable against a list of expected values)
+        //In this case, it's the operations
         switch (operation) {
-            case 'addition':
-                let resultAddition = a + b;
-                alert(resultAddition);
-            break;
-            case 'substraction':
-                let resultSubstraction = a - b;
-                alert(resultSubstraction);
-            break;
-            case 'multiplication':
-                let resultMultiplication = a * b;
-                alert(resultMultiplication);
-            break;
-            case 'division':
-                let resultDivision = a / b;
-                alert(resultDivision);
-            break;
+            case "addition":
+                let sum = a + b;
+                alert(sum);
+                break;
+            case "substraction":
+                let substraction = a - b;
+                alert(substraction);
+                break;
+            case "multiplication":
+                let multiplication = a * b;
+                alert(multiplication);
+                break;
+            case "division":
+                let division = a / b;
+                alert(division);
+                break;
+            //No need for a default
         }
+
     };
 
-    Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
-        $btn.addEventListener(
-            "click",
-            () => (performOperation($btn.id), false),
-        ),
-    );
+    //An array is created from the elements under the ".operator" class. (from HTML code)
+    Array.from(document.querySelectorAll(".operator"))
+
+        //For each of these elements, a name is assigned (here, button) to which an EventListener is added.
+        //This EventListener is a click which launches a function which activates the function created above.
+        .forEach(button => button.addEventListener("click", () =>
+            (performOperation(button.id)) //This function takes in parameters (the ids of all the buttons) *
+        )
+        );
+    //* It's the same as if you made, document.getElementById (4 times for the operators) but summarized
 })();
