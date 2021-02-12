3e0262b6f0c414b313b9e12916fc21dd61befb67
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-13 00:00:51 +0100

    Update 4.4

--- 01.Season-1/04-dates/04-locale-date/script.js
+++ 01.Season-1/04-dates/04-locale-date/script.js
@@ -5,10 +5,16 @@
 (() => {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-    const today = new Date();
+    const dateOfTheDay = new Date();
 
-    let currentDate = new Intl.DateTimeFormat("en-GB",
-        { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit' }).format(today);
+    let currentDate = new Intl.DateTimeFormat("en-GB", {
+        weekday: "long",
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    }).format(dateOfTheDay);
 
     document.getElementById("target").innerHTML = currentDate;
 
