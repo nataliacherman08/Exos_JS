a1630c4fd32b61c5c164efc2d6c84d460c2e895c
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 23:07:29 +0100

    Update 4.1

--- 01.Season-1/04-dates/01-title-by-hour-one/script.js
+++ 01.Season-1/04-dates/01-title-by-hour-one/script.js
@@ -6,12 +6,12 @@
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    let date = new Date();
+    let hour = new Date();
 
-    if (date.getHours() < 18) {
-        document.getElementById('target').innerHTML = "Hello !";
+    if (hour.getHours() < 18) {
+        document.getElementById('target').innerHTML = "Hello";
     } else {
-        document.getElementById('target').innerHTML = "Good Evening !";
+        document.getElementById('target').innerHTML = "Good Evening";
     }
 
 })();
