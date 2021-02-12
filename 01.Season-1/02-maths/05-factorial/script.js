6e0b0f063b290675f08559b8707b609db5b603a5
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 17:47:00 +0100

    Update 2.5

--- 01.Season-1/02-maths/05-factorial/script.js
+++ 01.Season-1/02-maths/05-factorial/script.js
@@ -2,10 +2,22 @@
 // 02-maths/05-factorial/script.js - 2.5: Factorial
 
 (() => {
-    
-    // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", () => {
+
+        let inputNumber = parseInt(document.getElementById("number").value);
+
+        function factorial(n) {
+            if (isNaN(n)) {
+                return "Unknown";
+            } else if (n == 0) {
+                return 1;
+            } else {
+                return Math.abs(n) * factorial((Math.abs(n) - 1));
+            }
+        }
+
+        alert(factorial(inputNumber));
     });
 
 })();
