cee7940e0a786e6994238b19815ac3a416d01f00
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 13:07:54 +0100

    Update 1.4

--- 01.Season-1/01-base/04-asv-confirm/script.js
+++ 01.Season-1/01-base/04-asv-confirm/script.js
@@ -6,23 +6,25 @@
     console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
     console.log('Hello Woods programmer');
 
-    // YOUR CODE HERE
-    let age = prompt("How old are you ?");
-    let gender = prompt("What's your gender ?");
-    let town = prompt("In which town do you live ?");
 
-    alert("You are " + age + " years old. " +
-    "You are a " + gender + ". " +
-    "And you live in " + town + ". ");
 
-    if(confirm("Is that you ?")) {}
-    else {
-        let age = prompt("How old are you ?");
-        let gender = prompt("What's your gender ?");
-        let town = prompt("In which town do you live ?");
+    //1) Questions to the user
+    let age = prompt("How old are you?");
+    let gender = prompt("What's your gender?");
+    let town = prompt("Where do you live?");
 
-        alert("You are " + age + " years old. " +
-        "You are a " + gender + ". " +
-        "And you live in " + town + ". ");
+    //2) Display the collected responses in a dialog box
+    alert(`You are ${age}. You are a ${gender}. And you live in ${town}.`)
+
+    //3) Ask the user to confirm
+    let confirmation = prompt("Is this really you?");
+
+    //4)Conditions
+    if ((confirmation == "no") || (confirmation == "no, it's not")) {
+        //5) Re-run the process
+        let age = prompt("How old are you?");
+        let gender = prompt("What's your gender?");
+        let town = prompt("Where do you live?");
     }
+
 })();
