64002e858a5186b4353639de1e8b9a85886ca084
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 23:32:33 +0100

    Update first part of 4.3

--- 01.Season-1/04-dates/03-age-by-select/script.js
+++ 01.Season-1/04-dates/03-age-by-select/script.js
@@ -4,6 +4,16 @@
 
 (() => {
 
-    // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+
+        let dayOfBirth = document.getElementById("dob-day").value;
+        let monthOfBirth = document.getElementById("dob-month").value;
+        let yearOfBirth = document.getElementById("dob-year").value;
+
+        //1) Get the birthdate of the visitor
+        dateBirth = `${dayOfBirth} / ${monthOfBirth} / ${yearOfBirth}`;
+        alert(dateBirth);
+    });
 
 })();
