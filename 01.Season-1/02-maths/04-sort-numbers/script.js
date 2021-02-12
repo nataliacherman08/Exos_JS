0ce5c76947ca4aa5c908fb10583f2ca64cf1fada
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-12 15:54:35 +0100

    Update 2.4

--- 01.Season-1/02-maths/04-sort-numbers/script.js
+++ 01.Season-1/02-maths/04-sort-numbers/script.js
@@ -7,11 +7,22 @@
     document.getElementById("run").addEventListener("click", () => {
 
         let numbers = document.getElementById('numbers');
-        let value = numbers.value;
-        let array = value.split(',');
+        //Attribuate to this numbers a value so we can sort it
+        let valueOfNumbers = numbers.value;
+        //Create an array from this numbers with a value
+        //The split() method divides a string from a separator
+        // to provide an array of sub-strings.
+        let arrayOfNumbers = valueOfNumbers.split(' ,');
 
-        array.sort(function (a, b) { return parseInt(a) - parseInt(b) });
-        alert(array);
+
+        //The sort() method sorts the elements of an array, in the same array, and returns the array.
+        //By default, sorting is performed on array elements converted into character strings and
+        //sorted according to the values of UTF-16 code units of characters.
+
+        //Syntax: arr.sort(fonctionComparaison(a, b))
+        arrayOfNumbers.sort(function (a, b) { return parseInt(a) - parseInt(b) });
+
+        alert(arrayOfNumbers);
 
     });
 
