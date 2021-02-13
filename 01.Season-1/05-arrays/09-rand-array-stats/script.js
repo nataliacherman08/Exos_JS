c750f48902080a40d2404866083b5a02d4d48ac8
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-13 01:37:09 +0100

    Update 5.9 first part

--- 01.Season-1/05-arrays/09-rand-array-stats/script.js
+++ 01.Season-1/05-arrays/09-rand-array-stats/script.js
@@ -2,6 +2,16 @@
 
 (() => {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+
+        //1) Display the ten elements of an array of 10 numbers (between 1 and 100) in the table
+        let arrayOfTenNumbers = [];
+
+        for (let i = 1; i < 11; i++) {
+            arrayOfTenNumbers.push(Math.floor(Math.random() * 100));
+            document.getElementById("n-" + i).innerHTML = arrayOfTenNumbers[arrayOfTenNumbers.length - 1];
+        }
+
+    });
 
 })();
