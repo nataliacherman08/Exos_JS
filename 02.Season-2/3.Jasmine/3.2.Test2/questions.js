765415c1396dd29af99eeb77993209099a087f4e
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-03 15:49:49 +0100

    Update

--- 02.Season-2/3.Jasmine/3.2.Test2/questions.js
+++ 02.Season-2/3.Jasmine/3.2.Test2/questions.js
@@ -1,43 +1,60 @@
-let CreationTableauLangages =  () => {
-
+//Create an array------------------------------------------------------------
+let CreationTableauLangages = () => {
+    return ["Html", "CSS", "Java", "PHP"];
 }
 
-let CreationTableauNombres =  () => {
-
+let CreationTableauNombres = () => {
+    return [0, 1, 2, 3, 4, 5];
 }
 
-let RemplacementElement =  (langages) => {
 
+//Replace  .splice(index, numberOfElement, "byWhat")--------------------------
+let RemplacementElement = (langages) => {
+    langages.splice(2, 1, "Javascript");
+    return langages;
 }
 
-let AjoutElementLangages =  (langages) => {
 
+//Add an element in an array--------------------------------------------------
+let AjoutElementLangages = (langages) => {
+    langages.push("Ruby", "Python");
+    return langages;
 }
 
-let AjoutElementNombres =  (nombres) => {
-
+let AjoutElementNombres = (nombres) => {
+    nombres.unshift(-2, -1);
+    return nombres;
 }
 
-let SuppressionPremierElement =  (langages) => {
 
+//Delete an element------------------------------------------------------------
+let SuppressionPremierElement = (langages) => {
+    langages.shift();
+    return langages;
 }
 
-let SuppressionDernierElement =  (langages) => {
-
+let SuppressionDernierElement = (langages) => {
+    langages.pop();
+    return langages;
 }
 
-let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
 
+//Convert a string in elements-------------------------------------------------
+let ConversionChaineTableau = (reseaux_sociaux_chaine) => {
+    return reseaux_sociaux_chaine.split(",");
 }
 
-let ConversionTableauChaine =  (langages) => {
-
+//Convert the elements of an array into a string ------------------------------
+let ConversionTableauChaine = (langages) => {
+    return langages.join(",");
 }
 
-let TriTableau =  (reseaux_sociaux) => {
-
+//Sort an array ----------------------------------------------------------------
+let TriTableau = (reseaux_sociaux) => {
+    return reseaux_sociaux.sort();
 }
 
-let InversionTableau =  (langages) =>{
-
+//Reverse an array ------------------------------------------------------------
+let InversionTableau = (langages) => {
+    return langages.reverse();
 }
