4b6b8e7dbabab0802b880126b99003de7200a3eb
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-03 13:18:51 +0100

    Update

--- 02.Season-2/3.Jasmine/3.1.Test1/questions.js
+++ 02.Season-2/3.Jasmine/3.1.Test1/questions.js
@@ -1,62 +1,100 @@
+//-----------------------Examples----------------------------------------------
 
 let tailleString = (texte) => {
     return texte.length;
 }
+
 let remplaceECar = (texte) => {
     let a = texte.indexOf("e")
-    let text =  texte.substr(" ", a) + " " + texte.substr(a + 1 , texte.length);
+    let text = texte.substr(" ", a) + " " + texte.substr(a + 1, texte.length);
     return text;
 }
+
+
+//------------------------Exercices------------------------------------------------------------------------------
+
+//Concat text
 let concatString = (texte1, texte2) => {
-
-}
-let afficherCar5 =  (texte) => {
-
-}
-let afficher9Car =  (texte) => {
-
-}
-let majusculeString =  (texte) => {
-
-}
-let minusculeString =  (texte) => {
-
-}
-let SupprEspaceString =  (texte) => {
-
-}
-let IsString =  (texte) => {
-
+    let finalText = texte1 + texte2;
+    return finalText;
 }
 
-let AfficherExtensionString =  (texte) => {
-
-}
-let NombreEspaceString =  (texte) => {
-
-}
-let InverseString =  (texte) => {
-
+//Display Strings
+let afficherCar5 = (texte) => {
+    let car5 = texte[4];
+    return car5;
 }
 
-/**
- * Exercices sur les nombres et les caluls mathématiques
- */
-let calculPuissance =  (x, y) => {
-
+let afficher9Car = (texte) => {
+    let car9 = texte.substr(0, 9);
+    return car9;
 }
-let valeurAbsolue =  (nombre) => {
 
+//UpperCase
+let majusculeString = (texte) => {
+    return texte.toUpperCase();
 }
-let valeurAbsolueArray =  (array) => {
 
+//LowerCase
+let minusculeString = (texte) => {
+    return texte.toLowerCase();
 }
-let sufaceCercle =  (rayon) => {
 
+//Delete space
+let SupprEspaceString = (texte) => {
+    return texte.trim();
 }
-let hypothenuse =  (ab, ac) => {
 
+//Check the type
+let IsString = (texte) => {
+    return typeof texte === "string" ? true : false;
 }
-let calculIMC =  (poids, taille) => {
 
+let AfficherExtensionString = (texte) => {
+    let lastindexof = texte.lastIndexOf('.');
+    return texte.substr(lastindexof + 1);
+}
+
+let NombreEspaceString = (texte) => {
+    return texte.split(" ").length - 1;
+}
+
+let InverseString = (texte) => {
+    return texte.split("").reverse().join("");
+}
+
+/*
+-------------------------------------- Exercices sur les nombres et les caluls mathématiques ------------------------------------------------------*/
+
+let calculPuissance = (x, y) => {
+    return Math.pow(x, y);
+}
+
+//Calculate the absolute value
+let valeurAbsolue = (nombre) => {
+    return Math.abs(nombre);
+}
+
+
+let valeurAbsolueArray = (array) => {
+    let arr = [];
+    array.forEach(e => {
+        arr.push(Math.abs(e));
+    });
+    return arr;
+}
+
+//Calculate the surface of a Circle
+let sufaceCercle = (rayon) => {
+    return Math.round(Math.PI * Math.pow(rayon, 2));
+}
+
+//Calculate the hypothenuse
+let hypothenuse = (ab, ac) => {
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
+}
+
+//Calculate the IMC
+let calculIMC = (poids, taille) => {
+    return parseFloat((poids / Math.pow(taille, 2)).toFixed(2));
 }
